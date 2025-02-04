ffc353b5f395811cd3ed05756fbb1d798e9e9ded
ellennamation <ellennamation@gmail.com>
2025-02-04 13:53:21 -0600

    Added enemy information

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,8 +6,10 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     public Transform player;
-    public int health = 5; 
+    public int health = 5;
     private NavMeshAgent navMeshAgent;
+    public Transform healthbarRect; 
+    private float initHealthX;
 
     void Start()
     {
@@ -30,31 +32,42 @@ public class EnemyMovement : MonoBehaviour
 
             //adjust healthbar
             health -= 1;
+
+            //width
+            Vector3 newSize = healthbarRect.localScale;
+            newSize.x = health * 0.04f;
+            healthbarRect.localScale = newSize;
+
+            //position x
+            Vector3 newPosition = healthbarRect.position;
+            newPosition.x = health * 0.2f; 
+            healthbarRect.position = newPosition; 
+
             print("hit!");
 
             if (health == 0)
             {
                 //change color
-                print("Enemy dead");
+                Destroy(this.gameObject);
             }
         }
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("projectile"))
-        {
-            Destroy(other.gameObject);
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    if (other.gameObject.CompareTag("projectile"))
+    //    {
+    //        Destroy(other.gameObject);
 
-            //adjust healthbar
-            health -= 1;
-            print("hit!");
+    //        //adjust healthbar
+    //        health -= 1;
+    //        print("hit!");
 
-            if (health == 0)
-            {
-                //change color
-                print("Enemy dead");
-            }
-        }
-    }
+    //        if (health == 0)
+    //        {
+    //            //change color
+    //            print("Enemy dead");
+    //        }
+    //    }
+    //}
 }
